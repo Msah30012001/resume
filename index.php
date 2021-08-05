d785655819611482b5aec2468d17f714020741bc
Manish Shambhoo Sah <56181606+Msah30012001@users.noreply.github.com>
2021-08-05 19:09:15 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -81,9 +81,7 @@
                 <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print
                     media. I enjoy turning complex problems into simple, beautiful and intuitive designs. My job is to
                     build your website so that it is functional and user-friendly but at the same time attractive.
-                    Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use.
-                    My aim is to bring across your message and identity in the most creative way. I created web design
-                    for many famous brand companies.</p>
+                    Moreover,.</p>
                 <a href="#!" class="btn btn-primary btn-hire-me">HIRE ME</a>
             </section>
             <section class="resume-section">
